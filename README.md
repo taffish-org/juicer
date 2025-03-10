ed292d590539964b4962858dab33a1e848dbd7e0
Kaiyuan Han <hermit_han@163.com>
2025-03-10 22:43:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-juicer
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/aidenlab/juicer
+
+A One-Click System for Analyzing Loop-Resolution Hi-C Experiments.
---
+++ juicer.info.lisp
@@ -0,0 +1,12 @@
+(:name           "juicer")
+(:repo           "taffish-org/juicer")
+(:author         "KaiyuanHan")
+(:version        "2.20.00")
+(:intro          "A One-Click System for Analyzing Loop-Resolution Hi-C Experiments.")
+(:dependencies)
+(:functions      "java"
+                 "[juicer] dump"
+                 "[juicer] pre"
+                 "[juicer] ab"
+                 "[juicer] tad"
+                 "[juicer] loop")
